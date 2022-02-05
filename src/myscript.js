f36526be7df866d26ce00e8698c56f1dde638ec9
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 12:05:32 +0530

    added console log

--- src/myscript.js
+++ src/myscript.js
@@ -13,28 +13,53 @@ var StudlyCapVar;
 var ProperCamelCase;
 // Variable assignments
 
-studlyCapVar = 10
+studlyCapVar = 10;
+console.log(studlyCapVar);
 properCamelCase='A String';
+console.log(properCamelCase);
 titleCaseOver=9000;
+console.log(titleCaseOver);
 TitleCaseOver = 9000;
+console.log(TitleCaseOver);
 StudlyCapVar = 10;
+console.log(StudlyCapVar);
 ProperCamelCase = "A String";
+console.log(ProperCamelCase);
 let catName = "Oliver";
+console.log(catName);
 let catSound = "Meow!";
+console.log(catSound);
 const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
-console.log(FCC, fact); // Change this line
+console.log(FCC);
+console.log(fact);// Change this line
 const sum = 10 + 10;
+console.log(sum);
 const difference = 45 - 33;
-const product = 8 * 10;
+console.log(difference);
+const prduct = 8 * 10;
+console.log(prduct);
 const quotient = 66 / 20;
+console.log(quotient);
 let myVar = 87;
+
 
 // Only change code below this line
 myVar = ++myVar;
+console.log(myVar);
 myVar=--myVar;
+console.log(myVar);
 const myDecimal= 4.7;
+console.log(myDecimal);
 const product = 2.0 * 3.0;
-const product = 2.0 / 3.0;
+
+console.log(product);
+const prouct = 2.0 / 3.0;
+console.log(prouct);
 const a = 2 % 3;
+console.log(a);
+let b = 3;
+b+=2;
+console.log(b);
+

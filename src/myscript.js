20db04f2b7dfb8f20e66aa60be9007ec782e4bb5
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 15:16:40 +0530

    card game

--- src/myscript.js
+++ src/myscript.js
@@ -1,11 +1,45 @@
 
 function fun() {
-    let a= fun1();
-    console.log(a);
+    var count = 0;
+    cc(2); cc(3); cc(7); cc('K'); cc('A');
+    var a =cc();
+    function cc(card) {
 
-  function fun1(){
-      return false;
-      //return early pattern for function
-      console.log('bye');
-  }
+    
+      var msg = '';
+    
+      switch (card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+          count++;
+          break;
+        case 7:
+        case 8:
+        case 9:
+          count = count;
+          break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+          count--;
+          break;
+      }
+    
+      if (count === 5) msg = "5 Bet";
+      else if (count === 0) msg = "0 Hold";
+      else if (count === -5) msg = "-5 Hold";
+      else if (count === -1) msg = "-1 Hold";
+      else if (count === 1) msg = "1 Bet";
+    
+    
+      return msg;
+      
+    }
+    console.log(a);
+    
 }

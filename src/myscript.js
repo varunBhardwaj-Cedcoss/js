1477a6e372bc9caad83dccd72834aea91c5098e5
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 15:02:51 +0530

    replace else with switch

--- src/myscript.js
+++ src/myscript.js
@@ -2,6 +2,13 @@
 function fun() {
 
     let ch = "c";
+   /* if (ch === 'a') {
+        answer = "a";
+      } else if (ch === 'b') {
+        answer = "b";
+      } else {
+        answer = "c";
+      }*/
     switch (ch) {
         case "a":
         case"c":

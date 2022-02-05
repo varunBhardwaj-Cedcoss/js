2dc18407291bbf650762ee168874d283b1408d94
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 12:53:24 +0530

    push element in array

--- src/myscript.js
+++ src/myscript.js
@@ -81,11 +81,7 @@ const p = s[y-2];
 console.log(p);
 const wb = s+' '+m+''+f;
 console.log(wb);
-const arr = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [[10, 11, 12], 13, 14]
-  ];
-;
-console.log(arr[3][0]);
+const arr = ['varun'];
+  arr.push('bhardwaj');
+console.log(arr);
+

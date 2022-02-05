69376a80ff712add84b8621fdb7c302b3863eb13
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 14:00:16 +0530

    comparision logic'

--- src/myscript.js
+++ src/myscript.js
@@ -1,8 +1,12 @@
 
 function fun(){
-    let a=5;
+    let a=50;
+    let b=0;
     if(a==5){
         return(console.log(true));
+    }
+    if(b==0){
+        return(console.log(a));
     }
    else{
     return(console.log(false));

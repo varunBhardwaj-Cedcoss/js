1b89bb06e26ebe0b71a4c08e173a0e7cf24661b7
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 13:36:33 +0530

    global vs local

--- src/myscript.js
+++ src/myscript.js
@@ -1,18 +1,18 @@
-// Declare the myGlobal variable below this line
+//global var declaration
 const myGlobal = 10;
 
 function fun1() {
   // Assign 5 to oopsGlobal Here 
 const oopsGlobal = 5;
+return funct(oopsGlobal);
 //local scope of this var
 }
 
 // Only change code above this line
 
-function funct() {
-  var output;
+function funct(a) {
+  var output='';
     output += myGlobal;
-    fun1()
-    output += oopsGlobal;
+    output += a;
   return(console.log(output));
 }

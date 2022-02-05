269bdf930a170983ac9d8768cbec6c2e0b2d3a19
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 15:39:29 +0530

    testing object

--- src/myscript.js
+++ src/myscript.js
@@ -1,23 +1,8 @@
 
 function fun() {
-    let a= phoneticLookup("charlie");
-    function phoneticLookup(val) {
-        var result = "";
-      
-        var lookup = {
-          "alpha": "Adams",
-          "bravo": "Boston",
-          "charlie": "Chicago",
-          "delta": "Denver",
-          "echo": "Easy",
-          "foxtrot": "Frank"
-        }; 
-      
-        result = lookup[val];
-      
-        return result;
-      }
-      console.log(a);
-      
+    let a = {firstName:"varun", lastName:"bhardwaj"};
+    let b=a.hasOwnProperty('firstName');
+   
+    console.log(b);
     
 }

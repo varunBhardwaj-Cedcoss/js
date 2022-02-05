74818eace7f9cfe90d5a37663abd2974650b861f
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 11:08:47 +0530

    Declare a Read-Only Variable with the const Keyword

--- src/myscript.js
+++ src/myscript.js
@@ -21,3 +21,7 @@ StudlyCapVar = 10;
 ProperCamelCase = "A String";
 let catName = "Oliver";
 let catSound = "Meow!";
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line

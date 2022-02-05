f5d7ee5b3467fcfc3ed54b2f6e035ecd9687a04b
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 10:55:28 +0530

    understanding unintialize var

--- src/myscript.js
+++ src/myscript.js
@@ -4,8 +4,11 @@ multi
 line
 comment
 */
-var myName;
-var a=7;
-var b=8;
-var myFirstname='varun';
-var myLastName='bhardwaj';
+var a;
+a=5;
+var b;
+b=10;
+var c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";

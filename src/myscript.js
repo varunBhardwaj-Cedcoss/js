6e0d048933eeba15a25835ec62e848e53412b0db
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 13:56:43 +0530

    boolean value added

--- src/myscript.js
+++ src/myscript.js
@@ -1,16 +1,7 @@
 
 function fun(){
-const testArr = [1, 2, 3, 4, 5];
+    let a=true;
+   return(console.log(a));
 
-  console.log("Before: " + testArr);
-  console.log(nextInLine(testArr, 6));
-  console.log("After: " + testArr);
+
 }
-function nextInLine(arr, item) {
-  
-    let a=arr[0];
-    arr.push(item);
-    arr.shift();
-    return a; 
-  
-  }

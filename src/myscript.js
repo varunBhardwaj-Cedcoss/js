dfce92a4f9b9c74efb21a151e164ac51253dc162
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 15:52:50 +0530

    accessing nested array

--- src/myscript.js
+++ src/myscript.js
@@ -1,17 +1,28 @@
 
 function fun() {
-    var myStorage = {
-        "car": {
-          "inside": {
-            "glove box": "maps",
-            "passenger seat": "crumbs"
-           },
-          "outside": {
-            "trunk": "jack"
-          }
-        }
-      };
-      var gloveBoxContents = myStorage.car.inside["glove box"]; 
-    console.log(gloveBoxContents);
+// Setup
+var myPlants = [
+    {
+      type: "flowers",
+      list: [
+        "rose",
+        "tulip",
+        "dandelion"
+      ]
+    },
+    {
+      type: "trees",
+      list: [
+        "fir",
+        "pine",
+        "birch"
+      ]
+    }
+  ];
+  
+  // Only change code below this line
+  
+  var secondTree = myPlants[1].list[1]; // Change this line
+  console.log(secondTree);
     
 }

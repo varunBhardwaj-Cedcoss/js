cb5c2c425c4a8b97beca6d062105f4520e4082dc
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 15:26:51 +0530

    update object prop

--- src/myscript.js
+++ src/myscript.js
@@ -2,7 +2,7 @@
 function fun() {
     let a = {firstName:"varun", lastName:"bhardwaj"};
     //acces property with var
-    let b= a.firstName
-    console.log(b);
+    a.firstName='tarun';
+    console.log(a);
     
 }

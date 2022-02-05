1a0d9ce1ccb616f085490448ff29b1266588f8e0
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 11:10:12 +0530

    add two number

--- src/myscript.js
+++ src/myscript.js
@@ -25,3 +25,4 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
+const sum = 10 + 10;

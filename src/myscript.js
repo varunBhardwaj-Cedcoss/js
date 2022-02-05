df1bf3f6b1ded490c8ae67a083312a7aae17460d
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 14:30:55 +0530

    chaning if else

--- src/myscript.js
+++ src/myscript.js
@@ -3,16 +3,19 @@ function fun(){
     let a=50;
     let b='0';
     let c='varun';
-    if(a==50 || b==5){
+    if(a==5){
         return(console.log(true));
     }
     //logical order of if else
-    else if(c>='varun'){
+    else if(c=='vu'){
         return(console.log(a));
     }
     //else statement
-   else{
+   else if(a==0 && b==c){
     return(console.log(false));
+   }
+   else{
+       return(console.log('have fun'));
    }
 
 

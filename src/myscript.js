fb8697340f853b96c10ddd7d62b6d23bf26dde21
varun_Bhardwaj <varunbhardwaj@cedcoss.com>
2022-02-05 16:01:25 +0530

    itreation for loop for odd no

--- src/myscript.js
+++ src/myscript.js
@@ -1,10 +1,9 @@
 
 function fun() {
     let ourArray=[];
-    let i=0;
-    for(;i <= 5;) {
+    let i=1;
+    for(;i <10;i+=2) {
         ourArray.push(i);
-        i++;
       }
   console.log(ourArray);
     
